96135bc8faec106297c6b01954f40082b1bfaebc
osneiderpallares <osneider.pallares20@gmail.com>
2023-11-10 17:35:42 -0500

    logo menu

--- src/@core/layouts/components/horizontal/app-bar-content/index.js
+++ src/@core/layouts/components/horizontal/app-bar-content/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 // ** Next Import
 import Link from 'next/link'
 
@@ -29,7 +30,8 @@ const AppBarContent = props => {
         userAppBarBranding(props)
       ) : (
         <LinkStyled href='/'>
-          <svg width={34} viewBox='0 0 32 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
+          <img width={34} src='/images/logos/clinisoft.png' />
+          {/* <svg width={34} viewBox='0 0 32 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
             <path
               fillRule='evenodd'
               clipRule='evenodd'
@@ -56,7 +58,7 @@ const AppBarContent = props => {
               fill={theme.palette.primary.main}
               d='M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z'
             />
-          </svg>
+          </svg> */}
           <Typography variant='h4' sx={{ ml: 2.5, fontWeight: 700, lineHeight: '24px' }}>
             {themeConfig.templateName}
           </Typography>

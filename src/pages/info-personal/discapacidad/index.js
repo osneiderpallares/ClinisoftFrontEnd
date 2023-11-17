3d05ec962bf1fac50cbd6be66bb93d642e189583
jacaballero1 <jkan87@hotmail.com>
2023-11-17 08:36:29 -0500

    Traduccion en tabla discapacidad

--- src/pages/info-personal/discapacidad/index.js
+++ src/pages/info-personal/discapacidad/index.js
@@ -294,7 +294,7 @@ const AppPage = ({}) => {
           onChange: event => handleSearch(event.target.value)
         }
       }}
-      localeText={GridLocaleTextES}
+      localeText={GridLocaleTextES()}
     />
   )
 
@@ -303,7 +303,7 @@ const AppPage = ({}) => {
   return (
     <Card>
       <CardHeader
-        title={t('Discapacidad')}
+        title={t('Disability')}
         action={
           <Tooltip title={t('Add')}>
             <Fab color='primary' aria-label='Add' size='small' onClick={openModal}>
